2ffd415f535b05f213ae7b56f3d2cee9b6fd980e
Ethan Salonga <ethansalonga28@gmail.com>
2022-05-09 19:51:18 -0400

    Error and exception handling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
+const { errorHandler } = require("./middleware/errorMiddleware")
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get("/api/users", (req, res) => {
   res.status(200).json({
@@ -12,5 +16,7 @@ app.get("/api/users", (req, res) => {
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
