071109e78332726c226bd70ef354071187c01087
Ethan Salonga <ethansalonga28@gmail.com>
2022-05-17 11:29:05 -0400

    Hook register form to redux

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,18 +1,31 @@
-import {createSlice, createAsyncThunk} from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 
 const initialState = {
   user: null,
   isError: false,
   isSuccess: false,
   isLoading: false,
-  message: ""
+  message: "",
 }
+
+// Register new user
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    console.log(user)
+  }
+)
+
+// Login user
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  console.log(user)
+})
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {}
+  extraReducers: (builder) => {},
 })
 
 export default authSlice.reducer
