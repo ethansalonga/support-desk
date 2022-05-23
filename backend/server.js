f97ce57b8e8e590d34c5283c9609818725becd5e
Ethan Salonga <ethansalonga28@gmail.com>
2022-05-23 12:26:24 -0400

    Ticket model & routes

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.get("/api/users", (req, res) => {
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"))
+app.use("/api/tickets", require("./routes/ticketRoutes"))
 
 app.use(errorHandler)
 
