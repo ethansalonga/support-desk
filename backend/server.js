c51a16b8f2a10f1a43afd75f1af29042998febcb
Ethan Salonga <ethansalonga28@gmail.com>
2022-05-28 11:15:15 -0400

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const express = require("express")
 const colors = require("colors")
 const dotenv = require("dotenv").config()
@@ -13,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get("/api/users", (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
     message: "Welcome to the Support Desk API",
   })
@@ -22,6 +23,22 @@ app.get("/api/users", (req, res) => {
 // Routes
 app.use("/api/users", require("./routes/userRoutes"))
 app.use("/api/tickets", require("./routes/ticketRoutes"))
+
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")))
+
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  )
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({
+      message: "Welcome to the Support Desk API",
+    })
+  })
+}
 
 app.use(errorHandler)
 
