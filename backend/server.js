facd86e0c0dfdea262044baebe52d014ce18b7b0
Ethan Salonga <ethansalonga28@gmail.com>
2022-05-29 18:53:37 -0400

    Server update

--- backend/server.js
+++ backend/server.js
@@ -14,29 +14,22 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Welcome to the Support Desk API",
-  })
-})
-
 // Routes
 app.use("/api/users", require("./routes/userRoutes"))
 app.use("/api/tickets", require("./routes/ticketRoutes"))
 
 // Serve Frontend
 if (process.env.NODE_ENV === "production") {
-  // Set build folder as static 
+  // Set build folder as static
   app.use(express.static(path.join(__dirname, "../frontend/build")))
 
-  app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
-  )
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+  })
 } else {
   app.get("/", (req, res) => {
-    res.status(200).json({
-      message: "Welcome to the Support Desk API",
-    })
+    res.status(200).json({ message: "Welcome to the Support Desk API" })
   })
 }
 
