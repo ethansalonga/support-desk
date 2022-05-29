c9193bc6bafcd53dbe782cb937c90f3f58daa00c
Ethan Salonga <ethansalonga28@gmail.com>
2022-05-29 14:15:35 -0400

    x

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use("/api/tickets", require("./routes/ticketRoutes"))
 
 // Serve Frontend
 if (process.env.NODE_ENV === "production") {
-  // Set build folder as static
+  // Set build folder as static 
   app.use(express.static(path.join(__dirname, "../frontend/build")))
 
   app.get("*", (req, res) =>
